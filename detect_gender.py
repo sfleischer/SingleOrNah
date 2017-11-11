ea5626af0a6e606d001f257b2e72fdab3267e89c
dcleung <derex.leung@gmail.com>
2017-11-11 01:03:23 -0500

    Starting code for detecting gender

---
+++ detect_gender.py
@@ -0,0 +1,100 @@
+# Adapted from Microsoft Azure Face API Quickstart Guide and  Documentation
+
+import httplib, urllib, base64, json
+
+# Replace the subscription_key string value with your valid subscription key.
+subscription_key = 'dfbf8eff05414406a5e0d33268caa0f9'
+
+# Replace or verify the region.
+#
+# You must use the same region in your REST API call as you used to obtain your subscription keys.
+# For example, if you obtained your subscription keys from the westus region, replace 
+# "westcentralus" in the URI below with "westus".
+#
+# NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
+# a free trial subscription key, you should not need to change this region.
+uri_base = 'westcentralus.api.cognitive.microsoft.com'
+
+# Request headers.
+headers = {
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': subscription_key,
+}
+
+# Request parameters.
+params = urllib.urlencode({
+    'returnFaceId': 'true',
+    'returnFaceLandmarks': 'false',
+    'returnFaceAttributes': 'gender',
+})
+
+# The URL of someone's profile picture to analyze.
+body = "{'url':'https://upload.wikimedia.org/wikipedia/commons/c/c3/RH_Louise_Lillian_Gish.jpg'}"
+
+try:
+    # Execute the REST API call and get the response.
+    conn = httplib.HTTPSConnection('westcentralus.api.cognitive.microsoft.com')
+    conn.request("POST", "/face/v1.0/detect?%s" % params, body, headers)
+    response = conn.getresponse()
+    data = response.read()
+
+    # 'data' contains the JSON data. The following formats the JSON data for display.
+    parsed = json.loads(data)
+    print ("Response:")
+    print (json.dumps(parsed, sort_keys=True, indent=2))
+    conn.close()
+
+except Exception as e:
+    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+####################################
+
+########### Python 3.6 #############
+import http.client, urllib.request, urllib.parse, urllib.error, base64, requests, json
+
+###############################################
+#### Update or verify the following values. ###
+###############################################
+
+# Replace the subscription_key string value with your valid subscription key.
+subscription_key = 'dfbf8eff05414406a5e0d33268caa0f9'
+
+# Replace or verify the region.
+#
+# You must use the same region in your REST API call as you used to obtain your subscription keys.
+# For example, if you obtained your subscription keys from the westus region, replace 
+# "westcentralus" in the URI below with "westus".
+#
+# NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
+# a free trial subscription key, you should not need to change this region.
+uri_base = 'https://westcentralus.api.cognitive.microsoft.com'
+
+# Request headers.
+headers = {
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': subscription_key,
+}
+
+# Request parameters.
+params = {
+    'returnFaceId': 'true',
+    'returnFaceLandmarks': 'false',
+    'returnFaceAttributes': 'gender',
+}
+
+# Body. The URL of a JPEG image to analyze.
+body = {'url': 'https://upload.wikimedia.org/wikipedia/commons/c/c3/RH_Louise_Lillian_Gish.jpg'}
+
+try:
+    # Execute the REST API call and get the response.
+    response = requests.request('POST', uri_base + '/face/v1.0/detect', json=body, data=None, headers=headers, params=params)
+
+    print ('Response:')
+    parsed = json.loads(response.text)
+    print (json.dumps(parsed, sort_keys=True, indent=2))
+
+except Exception as e:
+    print('Error:')
+    print(e)
+
+#################################### 
