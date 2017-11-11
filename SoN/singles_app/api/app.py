775bc987fcc7d2ea0e7315450335135de071c5e3
sfleischer <fls@seas.upenn.edu>
2017-11-11 07:35:35 -0500

    update api again

--- SoN/singles_app/api/app.py
+++ SoN/singles_app/api/app.py
@@ -166,7 +166,7 @@ class InstagramScraper(object):
             else:
                 return iter([])
         else:
-            time.sleep()
+            time.sleep(6)
             self.__query_comments(shortcode, end_cursor)
 
     def scrape_hashtag(self):
@@ -364,6 +364,7 @@ class InstagramScraper(object):
                 self.save_json(self.posts, '{0}/{1}.json'.format(dst, username))
 
         self.logout()
+        return self.mydata
 
     def get_profile_pic(self, dst, executor, future_to_item, user, username):
         # Download the profile pic if not the default.
@@ -624,11 +625,11 @@ class InstagramScraper(object):
                 place['location']['lng']
             ))
 
-    @staticmethod
-    def save_json(data, dst='./'):
+    def save_json(self, data, dst='./'):
         """Saves the data to a json file."""
-        if data:
-            print json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False)
+        self.mydata = data
+        # if data:
+        #     print json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False)
 
     @staticmethod
     def get_logger(level=logging.DEBUG, verbose=0):
@@ -670,100 +671,12 @@ class InstagramScraper(object):
 
 
 def start(kwargs):
-    # parser = argparse.ArgumentParser(
-    #     description="instagram-scraper scrapes and downloads an instagram user's photos and videos.",
-    #     epilog=textwrap.dedent("""
-    #     You can hide your credentials from the history, by reading your
-    #     username from a local file:
-    #     $ instagram-scraper @insta_args.txt user_to_scrape
-    #     with insta_args.txt looking like this:
-    #     -u=my_username
-    #     -p=my_password
-    #     You can add all arguments you want to that file, just remember to have
-    #     one argument per line.
-    #     """),
-    #     formatter_class=argparse.RawDescriptionHelpFormatter,
-    #     fromfile_prefix_chars='@')
-
-    # parser.add_argument('username', help='Instagram user(s) to scrape', nargs='*')
-    # parser.add_argument('--destination', '-d', default='./', help='Download destination')
-    # parser.add_argument('--login-user', '--login_user', '-u', default=None, help='Instagram login user')
-    # parser.add_argument('--login-pass', '--login_pass', '-p', default=None, help='Instagram login password')
-    # parser.add_argument('--login-only', '--login_only', '-l', default=False, action='store_true',
-    #                     help='Disable anonymous fallback if login fails')
-    # parser.add_argument('--filename', '-f', help='Path to a file containing a list of users to scrape')
-    # parser.add_argument('--quiet', '-q', default=False, action='store_true', help='Be quiet while scraping')
-    # parser.add_argument('--maximum', '-m', type=int, default=0, help='Maximum number of items to scrape')
-    # parser.add_argument('--retain-username', '--retain_username', '-n', action='store_true', default=False,
-    #                     help='Creates username subdirectory when destination flag is set')
-    # parser.add_argument('--media-metadata', '--media_metadata', action='store_true', default=False,
-    #                     help='Save media metadata to json file')
-    # parser.add_argument('--include-location', '--include_location', action='store_true', default=False,
-    #                     help='Include location data when saving media metadata')
-    # parser.add_argument('--media-types', '--media_types', '-t', nargs='+', default=['image', 'video', 'story'],
-    #                     help='Specify media types to scrape')
-    # parser.add_argument('--latest', action='store_true', default=False, help='Scrape new media since the last scrape')
-    # parser.add_argument('--tag', action='store_true', default=False, help='Scrape media using a hashtag')
-    # parser.add_argument('--filter', default=None, help='Filter by tags in user posts', nargs='*')
-    # parser.add_argument('--location', action='store_true', default=False, help='Scrape media using a location-id')
-    # parser.add_argument('--search-location', action='store_true', default=False, help='Search for locations by name')
-    # parser.add_argument('--comments', action='store_true', default=False, help='Save post comments to json file')
-    # parser.add_argument('--verbose', '-v', type=int, default=0, help='Logging verbosity level')
-
-    # args = parser.parse_args()
-
-    # if (args.login_user and args.login_pass is None) or (args.login_user is None and args.login_pass):
-    #     parser.print_help()
-    #     raise ValueError('Must provide login user AND password')
-
-    # if not args.username and args.filename is None:
-    #     parser.print_help()
-    #     raise ValueError('Must provide username(s) OR a file containing a list of username(s)')
-    # elif args.username and args.filename:
-    #     parser.print_help()
-    #     raise ValueError('Must provide only one of the following: username(s) OR a filename containing username(s)')
-
-    # if args.tag and args.location:
-    #     parser.print_help()
-    #     raise ValueError('Must provide only one of the following: hashtag OR location')
-
-    # if args.tag and args.filter:
-    #     parser.print_help()
-    #     raise ValueError('Filters apply to user posts')
-
-    # if args.filename:
-    #     args.usernames = InstagramScraper.parse_file_usernames(args.filename)
-    # else:
-    #     args.usernames = InstagramScraper.parse_delimited_str(','.join(args.username))
-
-    # if args.media_types and len(args.media_types) == 1 and re.compile(r'[,;\s]+').findall(args.media_types[0]):
-    #     args.media_types = InstagramScraper.parse_delimited_str(args.media_types[0])
-
-    # {'username': , 'media_types': ['image', 'video', 'story'], 'media_metadata': False, 'usernames': ['jhuanghuang'], 'verbose': 0, 'maximum': 0, 'login_user': None, 'login_only': False, 'destination': './', 'quiet': False, 'comments': False, 'filename': None, 'filter': None, 'search_location': False, 'tag': False, 'location': False, 'login_pass': None, 'retain_username': False, 'include_location': False, 'latest': False}
-
-    print(kwargs['username'])
 
     scraper = InstagramScraper(**kwargs)
 
-    # if args.tag:
-    #     scraper.scrape_hashtag()
-    # elif args.location:
-    #     scraper.scrape_location()
-    # elif args.search_location:
-    #     scraper.search_locations()
-    # else:
     scraper.scrape()
+    return json.dumps(scraper.posts, indent=4, sort_keys=True, ensure_ascii=False)
 
-# def trythis(args):
-#     scraper = InstagramScraper(**args)
-#     if args.tag:
-#         scraper.scrape_hashtag()
-#     elif args.location:
-#         scraper.scrape_location()
-#     elif args.search_location:
-#         scraper.search_locations()
-#     else:
-#         scraper.scrape()
 
 if __name__ == '__main__':
     #main()
