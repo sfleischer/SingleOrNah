ab637b0169ab1ea880e12ec24f73d57e6391749d
sfleischer <fls@seas.upenn.edu>
2017-11-11 07:52:56 -0500

    api with cap

--- SoN/singles_app/api/user.py
+++ SoN/singles_app/api/user.py
@@ -4,34 +4,18 @@ import json
 
 class User:
 
-    def __init__(self, user, username, password):
+    def __init__(self, user, username, password, cap=15):
         """
         Initializer for the class
         """
-        cap = 15; #50 images is the upper limit
         args = {'username': [user], 'media_types': ['none'], 'media_metadata': False, 'usernames': [user], 'verbose': 0, 'maximum': cap, 'login_user': username, 'login_only': False, 'destination': './', 'quiet': False, 'comments': True, 'filename': None, 'filter': None, 'search_location': False, 'tag': False, 'location': False, 'login_pass': password, 'retain_username': False, 'include_location': False, 'latest': False}
         self.scraper = app.start(args)
         self.json = json.loads(self.scraper)
+        #rint self.json
         #print self.json
 
 
     def isValidUser(self):
-        #TODO
-        pass
+        return self.json is None
 
-
-    def getImages(self):
-        #TODO
-        pass
-
-
-    def getProfilePicture(self):
-        #TODO
-        pass
-
-    def getComments(self):
-        #TODO
-        pass
-
-
-user = User("jhuanghuang", "puss_dragon8", "passwd")
+user = User("jhuanghuang", "puss_dragon8", "passwd", cap=1)
