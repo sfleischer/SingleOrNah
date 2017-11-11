0d6c46f00ced9d8de90a585f89ee20963b74bbba
Chris Fischer <chris.fischer36@gmail.com>
2017-11-11 09:05:18 -0500

    Added comment api stuff

--- SoN/singles_app/api/user.py
+++ SoN/singles_app/api/user.py
@@ -4,21 +4,20 @@ import json
 
 class User:
 
-    def __init__(self, user, username, password):
+    def __init__(self, user, username, password, cap=15):
         """
         Initializer for the class
         """
-        cap = 15; #50 images is the upper limit
+        cap = 30; #50 images is the upper limit
         args = {'username': [user], 'media_types': ['none'], 'media_metadata': False, 'usernames': [user], 'verbose': 0, 'maximum': cap, 'login_user': username, 'login_only': False, 'destination': './', 'quiet': False, 'comments': True, 'filename': None, 'filter': None, 'search_location': False, 'tag': False, 'location': False, 'login_pass': password, 'retain_username': False, 'include_location': False, 'latest': False}
         self.scraper = app.start(args)
         self.json = json.loads(self.scraper)
+        #rint self.json
         #print self.json
 
 
     def isValidUser(self):
-        #TODO
-        pass
-
+        return not self.json is None
 
     def getImages(self):
         #TODO
@@ -33,5 +32,9 @@ class User:
         #TODO
         pass
 
-
-user = User("jhuanghuang", "puss_dragon8", "passwd")
+if __name__ == "__main__":
+    user = User("0lonestar", "chrisdfisch", "passwd")
+    print (user.json)
+    for image in user.json:
+        comments = image['comments']['data']
+        print (image['edge_media_to_caption']['edges'][0]['node']['text'])
